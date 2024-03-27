c488247a55dd2711e21fc614439305da2a8cfed1
Cesar [Web&Blockchain] <75784005+cesar4design@users.noreply.github.com>
2024-03-27 19:39:54 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import type { WalletError } from '@tronweb3/tronwallet-abstract-adapter';
 import { WalletDisconnectedError, WalletNotFoundError } from '@tronweb3/tronwallet-abstract-adapter';
 // @ts-ignore
 import { toast } from 'react-hot-toast';
@@ -11,17 +10,7 @@ import { WalletModalProvider } from '@tronweb3/tronwallet-adapter-react-ui';
 import '@tronweb3/tronwallet-adapter-react-ui/style.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-    function onError(e: WalletError) {
-        if (e instanceof WalletNotFoundError) {
-            toast.error(e.message);
-        } else if (e instanceof WalletDisconnectedError) {
-            toast.error(e.message);
-        } else toast.error(e.message);
-    }
-    const adapters = useMemo(function () {
-        const tronLinkAdapter = new TronLinkAdapter();
-        return [tronLinkAdapter];
-    }, []);
+   
 
     /**
      * wrap your app content with WalletProvider and WalletModalProvider
@@ -31,7 +20,7 @@ export default function App({ Component, pageProps }: AppProps) {
      * Also you can provide a onError callback to process any error such as ConnectionError
      */
     return (
-        <WalletProvider onError={onError} adapters={adapters} disableAutoConnectOnLoad={true}>
+        <WalletProvider adapters={adapters} disableAutoConnectOnLoad={true}>
             <WalletModalProvider>
                 <Component {...pageProps} />
             </WalletModalProvider>
